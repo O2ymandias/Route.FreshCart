829891a308c4c2525884b9dbd27680bde7d20092
O2ymandias <ozymandias.ll.kmt@gmail.com>
2025-03-03 16:17:05 +0200

    Add Flowbite script to angular.json and initialize in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NgxSpinnerComponent } from 'ngx-spinner';
+import { FlowbiteService } from './core/services/flowbite.service';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,11 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private flowbite: FlowbiteService) {}
   title = 'E-Commerce';
+
+  ngOnInit(): void {
+    this.flowbite.loadFlowbite((flowbite) => {});
+  }
 }
